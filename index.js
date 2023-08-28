13d3ba41a8e503ade075e85b0f9a531e26c58a08
lukasbuksa <buksaluke@gmail.com>
2023-08-28 11:35:14 +0200

    assets path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 import browserSync from "browser-sync";
 import command from "./cli.js";
-import {config} from "./config.js";
+import { config } from "./config.js";
 import fs from "fs";
 
 command.parse(process.argv);
@@ -48,32 +48,27 @@ const scriptStyle = {
 };
 
 const rewriteRules = [
-    {...scriptStyle},
-    {...(options.blankMode && blankModeStyle)},
-    {...(options.blankMode && blankModeScript)},
+    { ...scriptStyle },
+    { ...(options.blankMode && blankModeStyle) },
+    { ...(options.blankMode && blankModeScript) },
 ];
 
 const bs = browserSync.create();
 bs.init({
-    proxy: {target: options.remote ?? config.defaultUrl},
+    proxy: { target: options.remote ?? config.defaultUrl },
     host: options.host ?? config.defaultHost,
     open: options.mode === "production" ? false : "local",
     watch: options.mode !== "production" ? options.watch : false,
     files:
         options.mode !== "production"
             ? [
-                options.folder
-                    ? "./" + options.folder + "/*"
-                    : config.defaultFolder + "/*",
-            ]
+                  options.folder
+                      ? "./" + options.folder + "/*"
+                      : config.defaultFolder + "/*",
+              ]
             : [],
-    serveStatic: [
-        options.folder ?? config.defaultFolder,
-        {
-            route: ["/assets"],
-            dir: "./build",
-        },
-    ],
+    serveStatic: [options.folder ?? config.defaultFolder],
+    snippet: false,
     rewriteRules: rewriteRules.filter(
         (value) => Object.keys(value).length !== 0
     ),
